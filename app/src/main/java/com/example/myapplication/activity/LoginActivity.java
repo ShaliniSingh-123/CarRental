6b1b475bbbe93c6af79c99c4850c96c89f970be7
ShaliniSingh-123 <shalinisingh954856@gmail.com>
2024-12-02 15:41:31 +0530

    2/12/24

--- app/src/main/java/com/example/myapplication/activity/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/activity/LoginActivity.java
@@ -50,7 +50,7 @@ public class LoginActivity extends AppCompatActivity {
         signupRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                Intent intent = new Intent(LoginActivity.this, SignupDashboardActivity.class);
                 startActivity(intent);
             }
         });
